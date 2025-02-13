5dd40c0c324204be14a88ea941786f8e3a243b05
Totengeist <me@totengeist.com>
2025-02-13 13:33:27 -0800

    Add Compatibility tests

---
+++ tests/CompatibilityTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests;
+
+use Totengeist\IVParser\CompatibilityMatrix;
+
+class CompatibilityTest extends TestCase {
+    public function testCanDiff() {
+        $matrix = new CompatibilityMatrix();
+
+        $this->assertEquals(array('test1', 'test2', 'test3'), $matrix->update_category(array('+' => array('test3')), array('test1', 'test2')));
+        $this->assertEquals(array('test1', 'test2', 'test3'), $matrix->update_category(array('-' => array('test4')), array('test1', 'test2', 'test3', 'test4')));
+        $this->assertEquals(array('test1', 'test2', 'test3'), $matrix->update_category(array('+' => array('test4'), '-' => array('test4')), array('test1', 'test2', 'test3')));
+        $this->assertEquals(array('test1', 'test2', 'test3', 'test4'), $matrix->update_category(array('+' => array('test4'), '-' => array('test4')), array('test1', 'test2', 'test3', 'test4')));
+        $this->assertEquals(array('test4', 'test5', 'test6'), $matrix->update_category(array('_' => array('test4', 'test5', 'test6')), array('test1', 'test2', 'test3')));
+        $this->assertEquals(array('test4', 'test5', 'test6'), $matrix->update_category(array('_' => array('test4', 'test5', 'test6'), '+' => array('test7'), '-' => array('test5')), array('test1', 'test2', 'test3')));
+    }
+
+    public function testCanLoadMatrix() {
+        $json = '
+{
+    "versions": {
+        "version1": {
+            "option1": {
+                "_": ["item1", "item2"]
+            }
+        },
+        "version2": {
+            "option1": {
+                "+": ["item3", "item4", "item4"],
+                "-": ["item1"]
+            },
+            "option2": {
+                "+": ["item1"]
+            }
+        },
+        "version3": {
+            "option1": {
+                "+": ["item3"],
+                "-": ["item3"]
+            },
+            "option2": {
+                "-": ["item1"]
+            }
+        }
+    }
+}
+';
+        $expected = array(
+            'version1' => array(
+                'option1' => array('item1', 'item2')
+            ),
+            'version2' => array(
+                'option1' => array('item2', 'item3', 'item4'),
+                'option2' => array('item1')
+            ),
+            'version3' => array(
+                'option1' => array('item2', 'item3', 'item4'),
+                'option2' => array()
+            )
+        );
+
+        $matrix = new CompatibilityMatrix();
+        $matrix->build_compatibility_matrix($json);
+        $this->assertEquals($expected, $matrix->matrix);
+    }
+
+    public function testCannotLoadMatrixWithoutVersions() {
+        $this->customExpectException('Totengeist\IVParser\Exception\InvalidFileException');
+        $json = '
+{
+    "blahblah": {}
+}
+';
+        $matrix = new CompatibilityMatrix();
+        $matrix->build_compatibility_matrix($json);
+    }
+}
